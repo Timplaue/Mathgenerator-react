ac2d327171c343dd777c281756f2812b0ed18018
Timplay <114745455+Timplaue@users.noreply.github.com>
2024-11-02 15:01:55 +0900

    02.11 latest version

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import './NavigationBar.css'; // Создайте этот файл для стилей
 
+
 const NavigationBar = ({ onProfileClick, onHomeClick }) => {
     return (
         <div className="navigation-bar">
             <button onClick={onHomeClick} className="nav-button">
-                <img src="path_to_home_icon.svg" alt="Home" /> {/* Замените на ваш путь к иконке */}
+                <img src="path_to_home_icon.svg" alt="Home"/> {/* Замените на ваш путь к иконке */}
                 Дом
             </button>
+            <button onClick={handleLogout}>Выйти</button>
             <button onClick={onProfileClick} className="nav-button">
-                <img src="path_to_profile_icon.svg" alt="Profile" /> {/* Замените на ваш путь к иконке */}
+                <img src="path_to_profile_icon.svg" alt="Profile"/> {/* Замените на ваш путь к иконке */}
                 Профиль
             </button>
         </div>
